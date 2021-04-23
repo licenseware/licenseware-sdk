f1b8a181d303d57aa1160aeaa321e9e6cfffb252
ClimenteA <climente.alin@gmail.com>
2021-04-23 14:40:27 +0300

    init pgk

--- src/licenseware/__init__.py
+++ src/licenseware/__init__.py
@@ -1,8 +1,8 @@
+from  . import mongodata
+from  . import decorators
 from .quota import Quota
-from . import mongodata
 from .auth import Authenticator
 from .notifications import notify_status
 from .file_validators import GeneralValidator, validate_filename
-from . import decorators
 from .registry import *
 from .utils import *
