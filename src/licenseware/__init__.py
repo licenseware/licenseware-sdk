0f8f93fb99f24a684e53e6781e64aebdc08c3cf5
ClimenteA <climente.alin@gmail.com>
2021-05-24 15:24:25 +0300

    fixed an import

--- src/licenseware/__init__.py
+++ src/licenseware/__init__.py
@@ -6,4 +6,5 @@ from .notifications import notify_status
 from .file_validators import GeneralValidator, validate_filename
 from .registry import *
 from .utils import *
+from .namespace_generator import *
 from .data_management import DataManagement
