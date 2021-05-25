c193f29144d2b789b32218456516f7b8dbb82314
ClimenteA <climente.alin@gmail.com>
2021-05-25 12:43:53 +0300

    added app BASE_PATH

--- src/licenseware/utils/__init__.py
+++ src/licenseware/utils/__init__.py
@@ -2,3 +2,4 @@ from .flask_utils import save_file, unzip, get_filepaths_from_event
 from .redis_service import RedisService
 from .mongodb_connection import get_mongo_connection
 from .redis_event_dispacher import RedisEventDispacher
+from .urls import *
