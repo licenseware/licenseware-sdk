2988f58e98ec428c5dd07de645cd80e028c4aa2c
ClimenteA <climente.alin@gmail.com>
2021-06-09 16:04:18 +0300

    fix: removed test setup mongodata

--- src/licenseware/mongodata.py
+++ src/licenseware/mongodata.py
@@ -33,18 +33,6 @@ from .decorators import failsafe
 from .utils.log_config import log
 
 
-MONGO_ROOT_USERNAME = 'licensewaredev'
-MONGO_ROOT_PASSWORD ='license123ware'
-MONGO_DATABASE_NAME='db'
-MONGO_HOSTNAME= 'localhost' #for a docker environment use 'mongodb' (service name)
-MONGO_PORT=27017
-
-os.environ['MONGO_DATABASE_NAME'] = MONGO_DATABASE_NAME
-os.environ['MONGO_CONNECTION_STRING'] = f"mongodb://{MONGO_ROOT_USERNAME}:{MONGO_ROOT_PASSWORD}@{MONGO_HOSTNAME}:{MONGO_PORT}"
-
-
-
-
 #Utils
 
 @failsafe
