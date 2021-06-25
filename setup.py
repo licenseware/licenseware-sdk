0174683b40c728b17bb3b1e2b79f31f8185a3900
ClimenteA <climente.alin@gmail.com>
2021-06-25 13:25:06 +0300

    feat: automatically get wheel version from CHANGELOG

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup, find_packages
+import re
+
 
 # Distribute py wheels
 # python3 setup.py bdist_wheel sdist
@@ -11,11 +13,27 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 with open("requirements.txt", "r") as fh:
-    requirements = fh.readlines()
+    REQUIREMENTS = fh.readlines()
+    
+    
+    
+VERSION = 'UNKNOWN'
+
+with open("CHANGELOG.md", "r") as fh:
+    changelog = fh.readlines()
+
+if len(changelog) > 2:    
+    raw_version = changelog[2]
+    version_match = re.search(r'### \[(\d+\.\d+\.\d+)\]', raw_version)
+    if version_match:
+        VERSION = version_match.group(1)
+
+
+
 
 setup(
     name="licenseware",
-    version="0.5.4",
+    version=VERSION,
     description="Common utilities for licenseware.",
     url="https://github.com/licenseware/licenseware-sdk",
     author="licenseware",
@@ -23,7 +41,7 @@ setup(
     license='',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires=requirements,
+    install_requires=REQUIREMENTS,
     package_dir={"": "src"},
     packages=find_packages(
         where="src",
