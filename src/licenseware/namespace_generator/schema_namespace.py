72f65b96ef5eaa90c27cbb55baf3be9a08c059b5
ClimenteA <climente.alin@gmail.com>
2021-06-28 10:24:19 +0300

    fix: namespace generator generates now only specified http methods

--- src/licenseware/namespace_generator/schema_namespace.py
+++ src/licenseware/namespace_generator/schema_namespace.py
@@ -9,173 +9,182 @@ from .mongo_request import MongoRequest
 
 
 auth_header_doc = {
-	'TenantId': {
-		'type': 'apiKey',
-		'in': 'header',
-		'name': 'TenantId'
-	},
-	'Authorization': {
-		'type': 'apiKey',
-		'in': 'header',
-		'name': 'Authorization'
-	}
+    'TenantId': {
+        'type': 'apiKey',
+        'in': 'header',
+        'name': 'TenantId'
+    },
+    'Authorization': {
+        'type': 'apiKey',
+        'in': 'header',
+        'name': 'Authorization'
+    }
 }
 
 
 allowed_methods = ['GET', 'POST', 'PUT', 'DELETE']
 
 class SchemaNamespace(MongoRequest):
-	"""
-		This class creates the Namespace object from Schema 
-		
-		DeviceNamespace = SchemaNamespace(
-			schema=DeviceSchema, 
-			collection='IFMPData', 
-			methods=['GET', 'POST']
-		)
+    """
+        This class creates the Namespace object from Schema 
+        
+        DeviceNamespace = SchemaNamespace(
+            schema=DeviceSchema, 
+            collection='IFMPData', 
+            methods=['GET', 'POST']
+        )
 
-	"""
+    """
 
-	# Used by namespace decorator
-	schema = None
-	collection = None
-	decorators = [authorization_check]
-	methods = allowed_methods # allowed methods(the rest will get a 405)
-	authorizations = auth_header_doc 
+    # Used by namespace decorator
+    schema = None
+    collection = None
+    decorators = [authorization_check]
+    methods = allowed_methods # allowed methods(the rest will get a 405)
+    authorizations = auth_header_doc 
 
-	def __init__(self, 
-	schema: Schema = None, 
-	collection: str = None, 
-	methods: list = allowed_methods, 
-	decorators: list = [authorization_check],
-	authorizations: dict = auth_header_doc
-	):
-		self.schema = self.schema or schema
-		self.collection = self.collection or collection
-		self.decorators = self.decorators or decorators
-		self.methods = self.methods or methods
-		self.authorizations = self.authorizations or authorizations
+    def __init__(self, 
+    schema: Schema = None, 
+    collection: str = None, 
+    methods: list = allowed_methods, 
+    decorators: list = [authorization_check],
+    authorizations: dict = auth_header_doc
+    ):
+        self.schema = self.schema or schema
+        self.collection = self.collection or collection
+        self.decorators = self.decorators or decorators
+        self.methods = self.methods or methods
+        self.authorizations = self.authorizations or authorizations
 
-		self.schema_name = self.schema.__name__
-		self.name = self.schema_name.replace("Schema", "")
-		self.path = "/" + self.name.lower()
-		self.json_schema = None
-		self.ns = None
-		self.model = None
-		self.resources = None
-		self.http_methods = None
-
-
-	@classmethod
-	def _initialize(cls):
-		c = cls(cls.schema, cls.decorators, cls.authorizations)
-		return c.initialize()
-
-	def __call__(self):
-		newcls = type(self.name, (SchemaNamespace,), {**self.__dict__})
-		return newcls._initialize()
-	
-	def initialize(self) -> Namespace:
-		""" Create restx api namespace from schema """
-
-		self.ns = self.create_namespace()
-		self.create_indexes()
-		self.make_json_schema()
-		self.model = self.ns.schema_model(self.name, self.json_schema)  
-		self.attach_http_methods()
-		self.attach_http_docs()
-		
-		self.resources = self.create_resources()
-		self.attach_resources()
-
-		return self.ns
-
-	def create_namespace(self) -> Namespace:        
-		ns = Namespace(
-			name = self.name, 
-			description = f"API is generated from {self.schema_name}",
-			decorators = self.decorators, 
-			authorizations = self.authorizations,
-			security = list(self.authorizations.keys())
-		)
-		return ns
+        self.schema_name = self.schema.__name__
+        self.name = self.schema_name.replace("Schema", "")
+        self.path = "/" + self.name.lower()
+        self.json_schema = None
+        self.ns = None
+        self.model = None
+        self.resources = None
+        self.http_methods = None
 
 
-	def create_resources(self) -> list:
+    @classmethod
+    def _initialize(cls):
+        c = cls(cls.schema, cls.decorators, cls.authorizations)
+        return c.initialize()
 
-		resource_list = []
-		for http_verb, dict_ in self.http_methods.items():
+    def __call__(self):
+        newcls = type(self.name, (SchemaNamespace,), {**self.__dict__})
+        return newcls._initialize()
+    
+    def initialize(self) -> Namespace:
+        """ Create restx api namespace from schema """
 
-			@self.ns.doc(**dict_['docs'])
-			class BaseResource(Resource): ...
-			
-			resource = type(
-				self.name + http_verb.capitalize(), 
-				(BaseResource,), 
-				{http_verb: dict_['method']}
-			)
+        self.ns = self.create_namespace()
+        self.create_indexes()
+        self.make_json_schema()
+        self.model = self.ns.schema_model(self.name, self.json_schema)  
+        self.attach_http_methods()
+        self.attach_http_docs()
+        
+        self.resources = self.create_resources()
+        self.attach_resources()
 
-			base_route = self.path + '/' + http_verb.lower()
-			routes = [base_route]
+        return self.ns
 
-			# TODO parameters from function parameters
-			# if 'params' in dict_['docs']:
-			#     print("-------- params:", dict_['docs']['params'])
-			#     for param in dict_['docs']['params']:
-			#         routes.append(base_route + '/' + param)
-
-			resource_list.append((resource, *routes))
-
-		return resource_list
-
-
-	def attach_resources(self):
-		for resource in self.resources:
-			self.ns.add_resource(*resource)
-
-
-	def attach_http_methods(self):
-		self.http_methods = {
-			"get"   : {'method': self.get,    'docs': None},
-			"post"  : {'method': self.post,   'docs': None},
-			"put"   : {'method': self.put,    'docs': None},
-			"delete": {'method': self.delete, 'docs': None}
-		} if self.http_methods is None else self.http_methods
+    def create_namespace(self) -> Namespace:        
+        ns = Namespace(
+            name = self.name, 
+            description = f"API is generated from {self.schema_name}",
+            decorators = self.decorators, 
+            authorizations = self.authorizations,
+            security = list(self.authorizations.keys())
+        )
+        return ns
 
 
-	def attach_http_docs(self):
+    def create_resources(self) -> list:
 
-		for k in self.http_methods.keys():
-			self.http_methods[k]['docs'] = {
-				'id': f'{k.upper()} request for {self.name}',
-				'description': f'Make a {k.upper()} request on {self.name} data.',
-				'responses':{
-					200: 'SUCCESS',
-					500: 'INTERNAL ERROR',
-					401: 'UNAUTHORIZED',
-					405: 'METHOD NOT ALLOWED',
-				},
-				'model':self.model,
-				'security': list(self.authorizations.keys())
-			}
+        resource_list = []
+        for http_verb, dict_ in self.http_methods.items():
 
-			if k in ['post', 'put', 'delete']:
-				self.http_methods[k]['docs'].update({'body':self.model})
+            @self.ns.doc(**dict_['docs'])
+            class BaseResource(Resource): ...
+            
+            resource = type(
+                self.name + http_verb.capitalize(), 
+                (BaseResource,), 
+                {http_verb: dict_['method']}
+            )
 
-			sig = signature(self.http_methods[k]['method'])
-			raw_params = list(dict(sig.parameters).keys())
-			
-			if raw_params:
-				self.http_methods[k]['docs'].update({"params": {}})
-				for param_name in raw_params:
-					param_type = re.search(r"<class '(.*?)'>", str(sig.parameters[param_name].annotation)).group(1)
-					param_type = 'str' if param_type == 'inspect._empty' else param_type
-					self.http_methods[k]['docs']["params"].update(
-						{ param_name:  f"Query parameter ({param_type})"}
-					)
+            base_route = self.path + '/' + http_verb.lower()
+            routes = [base_route]
 
-				
-	def make_json_schema(self) -> dict:
-		self.json_schema = JSONSchema().dump(self.schema())["definitions"][self.schema_name]
+            # TODO parameters from function parameters
+            # if 'params' in dict_['docs']:
+            #     print("-------- params:", dict_['docs']['params'])
+            #     for param in dict_['docs']['params']:
+            #         routes.append(base_route + '/' + param)
+
+            resource_list.append((resource, *routes))
+
+        return resource_list
+
+
+    def attach_resources(self):
+        for resource in self.resources:
+            self.ns.add_resource(*resource)
+
+
+    def attach_http_methods(self):
+     
+        if self.http_methods: return
+
+        methods = [m.lower() for m in self.methods]
+        
+        self.http_methods = {}
+        for method in methods:
+            if method == 'get':
+                self.http_methods[method] = {'method': self.get, 'docs': None}
+            if method == 'post':
+                self.http_methods[method] = {'method': self.post, 'docs': None}
+            if method == 'put':
+                self.http_methods[method] = {'method': self.put, 'docs': None}
+            if method == 'delete':
+                self.http_methods[method] = {'method': self.delete, 'docs': None}
+        
+
+    def attach_http_docs(self):
+
+        for k in self.http_methods.keys():
+            self.http_methods[k]['docs'] = {
+                'id': f'{k.upper()} request for {self.name}',
+                'description': f'Make a {k.upper()} request on {self.name} data.',
+                'responses':{
+                    200: 'SUCCESS',
+                    500: 'INTERNAL ERROR',
+                    401: 'UNAUTHORIZED',
+                    405: 'METHOD NOT ALLOWED',
+                },
+                'model':self.model,
+                'security': list(self.authorizations.keys())
+            }
+
+            if k in ['post', 'put', 'delete']:
+                self.http_methods[k]['docs'].update({'body':self.model})
+
+            sig = signature(self.http_methods[k]['method'])
+            raw_params = list(dict(sig.parameters).keys())
+            
+            if raw_params:
+                self.http_methods[k]['docs'].update({"params": {}})
+                for param_name in raw_params:
+                    param_type = re.search(r"<class '(.*?)'>", str(sig.parameters[param_name].annotation)).group(1)
+                    param_type = 'str' if param_type == 'inspect._empty' else param_type
+                    self.http_methods[k]['docs']["params"].update(
+                        { param_name:  f"Query parameter ({param_type})"}
+                    )
+
+                
+    def make_json_schema(self) -> dict:
+        self.json_schema = JSONSchema().dump(self.schema())["definitions"][self.schema_name]
 
 
