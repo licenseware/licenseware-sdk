9e133ccece3915e946888ad70034d1ac4c2144df
ClimenteA <climente.alin@gmail.com>
2021-07-02 10:04:07 +0300

    fix: moved stream cursor to 0 on file_validator

--- src/tests/test_validators.py
+++ src/tests/test_validators.py
@@ -6,100 +6,10 @@ import os
 
 
 # pytest -s tests/test_validators.py
-# pytest -s tests/test_validators.py::test_sccm
 
 
 test_files_path = "/home/acmt/Documents/files_test"
 
-
-def columns_check(file: str, reason: bool):
-    """
-        Checking if columns are present in the given file
-        If one case is valid the success result is returned 
-        If no case is valid a fail response is returned
-    """
-    
-    columns_software  = ['SoftwareKey', 'Publisher', 'Product', 'Version', 'Language', 'ProductCode']
-    columns_inventory = ['HardwareKey', 'SoftwareKey']
-    columns_hardware  = ['HardwareKey', 'Machine Name', 'Organizational Unit', 'IP Addresses',
-        'MAC Address', 'Has Client', 'Last Login', 'Operating System',
-        'Manufacturer', 'Model', 'Asset Tag', 'Number Of Processors',
-        'Serial Number', 'Processor Architecture', 'Processor Vendor',
-        'Procssor Brand', 'Domain', 'Hardware Class', 'Number of Cores',
-        'HT Capable', 'HT Enabled', 'Platform', 'Virtual Flag', 'Cluster Name',
-        'MultiUser Flag']
-
-    
-    valid_software_csv = GeneralValidator(
-        input_object=file,
-        text_contains_all=columns_software
-    ).validate(show_reason=reason)
-    
-    if reason:
-        if valid_software_csv['status'] == 'success': return valid_software_csv
-    else:
-        if valid_software_csv: return True
-    
-    
-    valid_inventory_csv = GeneralValidator(
-        input_object=file,
-        text_contains_all=columns_inventory
-    ).validate(show_reason=reason)
-    
-    if reason:
-        if valid_inventory_csv['status'] == 'success': return valid_inventory_csv
-    else:
-        if valid_inventory_csv: return True
-    
-    
-    valid_hardware_csv = GeneralValidator(
-        input_object=file,
-        text_contains_all=columns_hardware
-    ).validate(show_reason=reason)
-    
-    if reason:
-        if valid_hardware_csv['status'] == 'success': return valid_hardware_csv
-    else:
-        if valid_hardware_csv: return True
-        
-        
-    # If all files fail
-    if reason:
-        return {"status": "fail", "message": "File does not contain all the required columns needed for processing"}
-    else:
-        return False
-    
-    
-def test_sccm():
-    
-    files_path = os.path.join(test_files_path, "SCCM") 
-    files = ['HardwareQuery.csv', 'InventoryQuery.csv',  'SoftwareQueryMicrosoft.csv']
-    
-    for file in files:
-        res = columns_check(file=os.path.join(files_path, file), reason=False)
-        assert_that(res).is_true()
-    
-
-
-def test_powercli():
-    
-    columns_required = ['VCenterServerName', 'VCenterVersion',
-    'HostName','IsStandAlone', 'Datacenter',
-    'Cluster', 'VMs', 'Vendor',
-    'Model', 'FullName', 'Version',
-    'CpuModel', 'CpuMhz', 'CPU',
-    'CpuCores', 'HyperThreading', 'CpuThreads',
-    'PowerState', 'ConnectionState', 'Datastores']
-
-
-    v = GeneralValidator(
-        input_object = os.path.join(test_files_path, "PowerCLI/seskusvcenter-HW-Inventory-Information.csv"), 
-        text_contains_all = columns_required
-    )
-
-    assert_that(v.validate()).is_true()
-
-    
 
 
 def test_validate_filename():
