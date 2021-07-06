da129d2a640436f254748f0057b2827653da17e6
Ciprian <ciprian@licenseware.io>
2021-07-06 20:38:14 +0300

    feat: notifications decorator for event handler

---
+++ src/licenseware/decorators/notifications_event_handler.py
@@ -0,0 +1,34 @@
+from licenseware.utils.log_config import log
+from functools import wraps
+from licenseware.utils.redis_service import redis_connection as rd
+from licenseware.notifications import notify_status
+
+
+def send_notification(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        event = args[0]
+        number_key = f"number_of_running_events_{event['tenant_id']}_{event['event_type']}"
+        status_key = f"uploader_status_{event['tenant_id']}_{event['event_type']}"
+        current_status = rd.get(status_key)
+        if current_status == 'idle':
+            notify_status(
+                tenant_id=event['tenant_id'],
+                upload_id=event['event_type'],
+                status='running'
+            )
+            rd.set(status_key, 'running')
+        rd.incrby(number_key, 1)
+        result = await f(*args, **kwargs)
+        rd.decrby(number_key, 1)
+        currently_running = rd.get(number_key)
+        if currently_running == 0:
+            notify_status(
+                tenant_id=event['tenant_id'],
+                upload_id=event['event_type'],
+                status='idle'
+            )
+            rd.set(status_key, 'idle')
+        return result
+
+    return decorated
