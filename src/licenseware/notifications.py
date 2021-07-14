d66eb60d565ed907cd7147183ec335076f7ebd44
ClimenteA <climente.alin@gmail.com>
2021-07-14 15:08:44 +0300

    fix: backward_compatibility for notify_status function params

--- src/licenseware/notifications.py
+++ src/licenseware/notifications.py
@@ -17,11 +17,30 @@ from licenseware.utils.urls import NOTIFICATION_SERVICE_URL, APP_ID
 from licenseware.app_creator.tenant_utils import TenantUtils
 
 
+
+def backward_compatibility(**kparams):
+    #TODO notify_status needs only 2 params: event and status
+    #upload_id should be the same with event_type
+    #app_id should be unique Unique-service
+    # previous: event["tenant_id"], "ofmw_lms_collection", 'started', app_id='fmw-service'
+    # new: event, status
+    
+    if isinstance(kparams['tenant_id'], dict):
+        return kparams['tenant_id'], kparams['status'] 
+    else:
+        return kparams, kparams['status']
+
+
+
 @authenticated_machine
-async def notify_status(event, status):
+async def notify_status(tenant_id, upload_id=None, status=None, app_id=None): 
     """
         Sends a post request to registry service with the status of the data processing
     """
+    
+    event, status = backward_compatibility(
+        **dict(tenant_id=tenant_id, event_type=upload_id, status=status, app_id=app_id)
+    )
     
     if status == 'idle':
         # Check if there are any Running processsors before sending Idle to registry-service
@@ -40,7 +59,7 @@ async def notify_status(event, status):
         'tenant_id': event["tenant_id"],
         'upload_id': event["event_type"],
         'status': status,
-        'app_id': APP_ID
+        'app_id': app_id or APP_ID #added for backward compatibility
     }
 
     res = requests.post(
