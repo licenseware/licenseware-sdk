20212a427f316125bc5b58cb9dc651704d1c0c5f
Ciprian <ciprian@licenseware.io>
2021-07-19 13:32:26 +0300

    feat: added notifications directly in sdk

--- src/licenseware/notifications.py
+++ src/licenseware/notifications.py
@@ -9,62 +9,60 @@ or
 from licenseware import notify_status
 
 """
-import os, time
-import requests
+import os
+import time
+import traceback
 from licenseware.utils.log_config import log
 from licenseware.decorators.auth_decorators import authenticated_machine
 from licenseware.utils.urls import NOTIFICATION_SERVICE_URL, APP_ID
 from licenseware.app_creator.tenant_utils import TenantUtils
-
+from licenseware.notifications_handler.notifications_handler import EventNotificationsHandler
 
 
 def backward_compatibility(**kparams):
-    #TODO notify_status needs only 2 params: event and status
-    #upload_id should be the same with event_type
-    #app_id should be unique Unique-service
+    # TODO notify_status needs only 2 params: event and status
+    # upload_id should be the same with event_type
+    # app_id should be unique Unique-service
     # previous: event["tenant_id"], "ofmw_lms_collection", 'started', app_id='fmw-service'
     # new: event, status
-    
+
     if isinstance(kparams['tenant_id'], dict):
         return kparams['tenant_id'], kparams['status'] or kparams['event_type']
     else:
         return kparams, kparams['status'] or kparams['event_type']
 
 
-
 @authenticated_machine
-async def notify_status(tenant_id, upload_id=None, status=None, app_id=None): 
+async def notify_status(tenant_id, upload_id=None, status=None, app_id=None):
     """
         Sends a post request to registry service with the status of the data processing
     """
-    
+
     event, status = backward_compatibility(
         **dict(tenant_id=tenant_id, event_type=upload_id, status=status, app_id=app_id)
     )
-    
+
     if status == 'idle':
         # Check if there are any Running processsors before sending Idle to registry-service
-        wait = 5 #sec
-        for _ in range(int(3600/wait)): # 1 hour
-            res, _ = TenantUtils().get_uploader_status(event["tenant_id"], event['event_type'])
+        wait = 5  # sec
+        for _ in range(int(3600/wait)):  # 1 hour
+            res, _ = TenantUtils().get_uploader_status(
+                event["tenant_id"], event['event_type'])
             if res['status'] == 'Running':
                 time.sleep(wait)
             elif res['status'] == 'Idle':
                 break
-            
 
-    url = NOTIFICATION_SERVICE_URL + "/processing-status"
-    
-    payload = {
+    notif_event = {
         'tenant_id': event["tenant_id"],
         'upload_id': event["event_type"],
         'status': status,
-        'app_id': app_id or APP_ID.replace('worker', 'service') #added for backward compatibility
+        # added for backward compatibility
+        'app_id': app_id or APP_ID.replace('worker', 'service')
     }
 
-    res = requests.post(
-        url, json=payload, headers={"Authorization": os.getenv('AUTH_TOKEN')}
-    )
-    
-    if res.status_code != 200:
-        log.error("Failed to send processing status to notification-service")
+    try:
+        return EventNotificationsHandler(notif_event).status_check()
+    except Exception:
+        log.error(traceback.format_exc())
+        return None
