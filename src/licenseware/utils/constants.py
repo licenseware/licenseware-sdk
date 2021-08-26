75222905a64a832875019ee9b8ee02a0678640ef
Ciprian <ciprian@licenseware.io>
2021-08-26 14:14:50 +0300

    fix: handle app_id in environment instead of lware_identity

--- src/licenseware/utils/constants.py
+++ src/licenseware/utils/constants.py
@@ -1,6 +1,6 @@
 import os
 
-
+ENVIRONMENT=os.getenv("ENVIRONMENT", "production")
 BASE_PATH = os.getenv("APP_BASE_PATH", "")
 URL_PREFIX = os.getenv("APP_URL_PREFIX", "")
 BASE_URL = BASE_PATH + URL_PREFIX
@@ -9,7 +9,8 @@ REGISTRY_SERVICE_URL = os.getenv("REGISTRY_SERVICE_URL")
 
 NOTIFICATION_SERVICE_URL = os.getenv("NOTIFICATION_SERVICE_URL")
 
-APP_ID = os.getenv("LWARE_IDENTITY_USER", "")
+#APP_ID = os.getenv("LWARE_IDENTITY_USER", "")
+APP_ID = os.getenv("APP_ID", "") if 'local' in ENVIRONMENT else os.getenv("LWARE_IDENTITY_USER", "")
 
 
 UPLOAD_PATH = os.getenv("UPLOAD_PATH")
